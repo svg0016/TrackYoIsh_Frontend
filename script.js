6cdc5e43b16041406b7a40cebaeb54a544828981
Mr Windows SDE <f3153808@avmtk.com>
2021-03-18 15:43:04 -0500

    added quotes to onclick

--- script.js
+++ script.js
@@ -224,10 +224,10 @@ async function application() {
     let sideBarData = ``;
     if (data.ok) {
       data.trackingNumbers.forEach((element) => {
-        sideBarData += ` <div class="row"> <p1 onclick=${handleTrackingNumber(
+        sideBarData += ` <div class="row"> <p1 onclick="${handleTrackingNumber(
           null,
           { carrier: element.carrier, trackingNumber: element.trackingNumber }
-        )}>Tracking Number: ${element.number}</p1> </div>`;
+        )}">Tracking Number: ${element.number}</p1> </div>`;
       });
     }
     return sideBarData;
